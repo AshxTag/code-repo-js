49512810b6bb476513461432edc6ccfb2224eaef
AshxTag <132335364+AshxTag@users.noreply.github.com>
2024-10-20 05:30:40 +0000

    variables done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,7 +6,7 @@ let accountState
 accountEmail = "really@gmail.com"
 accountPass="156"
 accountCity="cheen"
-// accountId= 2 !--this is not allowed as it is a const value
+// accountId= 2 !--this is not allowed as it is a const value, const values dont change
 console.table([accountEmail,accountId,accountPass,accountCity,accountState])
 /*
 Don't use var for scope issues, use let instead.
