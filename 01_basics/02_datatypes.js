2e70fc662e3bb6cf99be1a99114b4d36863a4f8a
AshxTag <132335364+AshxTag@users.noreply.github.com>
2024-10-20 05:23:14 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat the code as newer version
+
+//alert(3+3) !--we are not in browser, we are using node js
+// !-- Code readablity should be high:
+console.log(3+3)
+
+console.log('real')
+
+let name = "Ashe" //string
+let age=10 //number
+let salary = 911.78//number
+let isLoggedIn = true //boolean
+
+//Datatypes:
+// PRIMITIVE:
+//number 2 to the power of 53
+//bigint !-- if the number is very big, for eg in trading/stocks
+// string !-- "" ''
+//boolean !-- true or false
+//null !-- standalone value, empty string
+//undefined !-- eg: var x;
+//symbol !--used in react
+
+//OBJECTS:
+
+console.log(typeof null);
+console.log(typeof undefined);
