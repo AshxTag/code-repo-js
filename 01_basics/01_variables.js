47b993935a394eb07ff8d2f9cae23c30d3521369
AshxTag <132335364+AshxTag@users.noreply.github.com>
2024-10-20 05:23:23 +0000

    datatypes done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123451
+let accountEmail ="imhim@gmail.com"
+var accountPass = "1234"
+accountCity = "China" //not suggested(way)to declare variables like this
+let accountState
+accountEmail = "really@gmail.com"
+accountPass="156"
+accountCity="cheen"
+// accountId= 2 !--this is not allowed
+console.table([accountEmail,accountId,accountPass,accountCity,accountState])
+/*
+Don't use var for scope issues, use let instead.
+*/
+
+
+
