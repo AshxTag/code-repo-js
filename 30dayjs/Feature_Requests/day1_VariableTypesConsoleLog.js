31aee6b82ee607ef8c2d7f0e7ae433bd7067baf0
AshxTag <132335364+AshxTag@users.noreply.github.com>
2024-10-20 06:37:43 +0000

    Feature Request 1 of day 1 completed

---
+++ 30dayjs/Feature_Requests/day1_VariableTypesConsoleLog.js
@@ -0,0 +1,16 @@
+// Variable Types Console Log: 
+// Write a script that declares variables of different data types and logs both the value and type of each variable to the console.
+let v1 = 10
+let v2 = "Kurapika"
+let v3 = false
+let v4 = null
+let v5 = [1,2,3,4,5]
+console.log(v1,typeof v1)
+console.log(v2,typeof v2)
+console.log(v3,typeof v3)
+console.log(v4,typeof v4)
+console.log(v5,typeof v5)
+
+
+
+
